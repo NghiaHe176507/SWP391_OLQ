57c1a1c2140f77404d1c9c32f5a06b3cd472bf62
NghiaHe176507 <121120257+NghiaHe176507@users.noreply.github.com>
2024-01-13 19:56:22 +0700

    Create project

---
+++ OnlineQuizProject/src/java/Dal/DBContext.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dal;
+
+import Entity.BaseEntity;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author nghia
+ */
+public abstract class DBContext<T extends BaseEntity> {
+
+    protected Connection connection;
+
+    public DBContext() {
+        try {
+            String url = "jdbc:sqlserver://localhost\\DESKTOP-KRG8A16\\NGHIA2003:1433;databaseName=Quiz;encrypt=true;trustServerCertificate=true";
+            String user = "sa";
+            String pass = "Nghia2003@@";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public abstract ArrayList<T> list();
+
+    public abstract void insert(T entity);
+
+    public abstract void update(T entity);
+
+    public abstract void delete(T entity);
+
+    public abstract T get(T entity);
+}
