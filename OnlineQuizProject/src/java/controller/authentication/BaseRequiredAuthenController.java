5c01d1b79f2b8f768221cc1099bffa929e8ecdae
DatShin35 <datshin353@gmail.com>
2024-01-23 16:24:13 +0700

    BaseRequiredAuthenController

---
+++ OnlineQuizProject/src/java/controller/authentication/BaseRequiredAuthenController.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller.authentication;
+
+import dal.AccountDBContext;
+import entity.Account;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Đạt Phạm
+ */
+@WebServlet(name = "BaseRequiredAuthenController", urlPatterns = {"/BaseRequiredAuthenController"})
+public abstract class BaseRequiredAuthenController extends HttpServlet {
+
+    private boolean isAuthenticated(HttpServletRequest request) {
+        Account account = (Account) request.getSession().getAttribute("account");
+       if (account != null) {
+            return true;
+        } else {
+            String mail = null;
+            String password = null;
+            Cookie[] cookies = request.getCookies();
+            for (Cookie cooky : cookies) {
+                if (mail != null && password != null) {
+                    break;
+                }
+                if (cooky.equals("mail")) {
+                    mail = cooky.getValue();
+                }
+                if (cooky.equals("password")) {
+                    password = cooky.getValue();
+                }
+            }
+
+            if (mail != null && password != null) {
+                AccountDBContext db = new AccountDBContext();
+                Account param = new Account();
+                param.setMail(mail);
+                param.setPassword(password);
+                account = db.get(param);
+                if (account != null) {
+                    request.getSession().setAttribute("account", account);
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (isAuthenticated(request)) {
+            //login
+            doGet(request, response, (Account) request.getSession().getAttribute("account"));
+        } else {
+           response.sendRedirect("access denied");
+        }
+    }
+
+   protected abstract void doGet(HttpServletRequest request, HttpServletResponse response, Account LoggedUser)
+            throws ServletException, IOException;
+
+    protected abstract void doPost(HttpServletRequest request, HttpServletResponse response, Account LoggedUser)
+            throws ServletException, IOException;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (isAuthenticated(request)) {
+            doPost(request, response, (Account) request.getSession().getAttribute("account"));
+        } else {
+            response.sendRedirect("login");
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- OnlineQuizProject/src/java/controller/authentication/LogoutController.java
+++ OnlineQuizProject/src/java/controller/authentication/LogoutController.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
-package controller.account;
+package controller.authentication;
 
 import java.io.IOException;
 import java.io.PrintWriter;
