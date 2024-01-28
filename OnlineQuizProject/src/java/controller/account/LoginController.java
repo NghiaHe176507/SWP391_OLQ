7ef74ddcd44aff300ff59161923e6fadd5e76685
DatShin35 <datshin353@gmail.com>
2024-01-28 21:22:42 +0700

    BaseRequireAuthor

--- OnlineQuizProject/src/java/controller/account/LoginController.java
+++ OnlineQuizProject/src/java/controller/account/LoginController.java
@@ -32,10 +32,21 @@ public class LoginController extends HttpServlet {
         String password = request.getParameter("password");
         AccountDBContext db = new AccountDBContext();
         Account account = db.getAccount(mail, password);
+<<<<<<< Updated upstream:OnlineQuizProject/src/java/controller/account/LoginController.java
         if (account != null) { //login succesful
             request.getSession().setAttribute("account", account);
             response.getWriter().println("Login successful");
             
+=======
+        Account param = new Account();
+        param.setMail(mail);
+        param.setPassword(password);
+        Account loggedUser = db.getAccount(mail, password);
+        
+          if (loggedUser == null ) {
+            request.setAttribute("checkAuthentication", "F");
+            request.getRequestDispatcher("view/login.jsp").forward(request, response);
+>>>>>>> Stashed changes:OnlineQuizProject/src/java/controller/authentication/LoginController.java
         } else {
             request.getSession().setAttribute("account", null);
             response.getWriter().println("Login failed");
