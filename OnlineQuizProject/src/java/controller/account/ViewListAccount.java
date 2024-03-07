8b9744a6da54cf7a4b67893743b98796c656e5af
DatShin35 <datshin353@gmail.com>
2024-03-08 01:07:27 +0700

    add pagging view list account

--- OnlineQuizProject/src/java/controller/account/ViewListAccount.java
+++ OnlineQuizProject/src/java/controller/account/ViewListAccount.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.account;
 
 import dal.ControllerDBContext;
@@ -22,27 +21,50 @@ import java.util.ArrayList;
  * @author PC
  */
 public class ViewListAccount extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
+        int pageSize = 4; // Number of items per page
+        int page = 1; // Default page number
+
+        if (request.getParameter("page") != null) {
+            page = Integer.parseInt(request.getParameter("page"));
+        }
         ControllerDBContext db = new ControllerDBContext();
         ArrayList<AccountInfo> listAccount = db.getListAccountWithInfo();
+
+        // Paginate the data
+        int totalItems = listAccount.size();
+        int totalPages = (int) Math.ceil((double) totalItems / pageSize);
+
+        int startIndex = (page - 1) * pageSize;
+        int endIndex = Math.min(startIndex + pageSize, totalItems);
+
+        ArrayList<AccountInfo> paginatedList = new ArrayList<>(listAccount.subList(startIndex, endIndex));
+
+
         ArrayList<RoleFeature> listRoleFeature = db.getListRoleFeatureByListAccount(listAccount);
         request.setAttribute("listAccountWithInfo", listAccount);
         request.setAttribute("listRoleFeatureByListAccount", listRoleFeature);
+        request.setAttribute("listStudent", paginatedList);
+        request.setAttribute("totalPages", totalPages);
+        request.setAttribute("currentPage", page);
         request.getRequestDispatcher("/view/controllerAccount/AccountManagement.jsp").forward(request, response);
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -50,12 +72,13 @@ public class ViewListAccount extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -63,12 +86,13 @@ public class ViewListAccount extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
