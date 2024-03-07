48168146d245c4abcd5283ea39b950ffe1407ef9
DatShin35 <datshin353@gmail.com>
2024-03-07 14:54:37 +0700

    Update DBContext.java

--- OnlineQuizProject/src/java/dal/DBContext.java
+++ OnlineQuizProject/src/java/dal/DBContext.java
@@ -22,9 +22,9 @@ public abstract class DBContext<T extends BaseEntity> {
 
    public DBContext() {
         try {
-            String url = "jdbc:sqlserver://localhost\\DESKTOP-KRG8A16\\NGHIA2003:1433;databaseName=Quiz;encrypt=true;trustServerCertificate=true";
+            String url = "jdbc:sqlserver://localhost\\DatShin35\\SQLEXPRESS:1433;databaseName=Quiz;encrypt=true;trustServerCertificate=true";
             String user = "sa";
-            String pass = "Nghia2003@@";
+            String pass = "03052003";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException ex) {
