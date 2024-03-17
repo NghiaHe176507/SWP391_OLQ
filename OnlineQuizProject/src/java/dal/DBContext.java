bc16a04598ee65f475e0c028cfdf1e6283b1db8e
MinhTuan2003-max <minhtuanha2829@gmail.com>
2024-03-17 22:42:25 +0700

    databse

--- OnlineQuizProject/src/java/dal/DBContext.java
+++ OnlineQuizProject/src/java/dal/DBContext.java
@@ -26,13 +26,13 @@ public abstract class DBContext<T extends BaseEntity> {
 //            String user = "thanbd";
 //            String pass = "123";
 
-//            String url = "jdbc:sqlserver://LAPTOP-TML7FNC1:1433;databaseName=Quiz;encrypt=true;trustServerCertificate=true";
-//            String user = "sa";
-//            String pass = "minhtuanha2003";
-
-            String url = "jdbc:sqlserver://localhost\\DESKTOP-KRG8A16\\NGHIA2003:1433;databaseName=Quiz;encrypt=true;trustServerCertificate=true";
+            String url = "jdbc:sqlserver://LAPTOP-TML7FNC1:1433;databaseName=Quiz;encrypt=true;trustServerCertificate=true";
             String user = "sa";
-            String pass = "Nghia2003@@";
+            String pass = "minhtuanha2003";
+
+//            String url = "jdbc:sqlserver://localhost\\DESKTOP-KRG8A16\\NGHIA2003:1433;databaseName=Quiz;encrypt=true;trustServerCertificate=true";
+//            String user = "sa";
+//            String pass = "Nghia2003@@";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException ex) {
